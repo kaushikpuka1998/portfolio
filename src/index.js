af826a8d8926c5caa538de14f3d3226c16d0a7fd
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2025-07-15 13:58:08 +0530

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider } from './components/context/ThemeContext';
+import {ThemeProvider} from './components/context/ThemeContext';
 import App from './app/App';
-import Themetoggle from './components/themetoggle';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <ThemeProvider>
-            <App />
+        <App/>
     </ThemeProvider>
 );
 
