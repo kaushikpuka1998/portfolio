9b40e856e4b6b47b6fc4ddd2857c991f91574e82
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2026-05-21 03:39:44 +0530

    Image position issue resolved

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -20,7 +20,7 @@ export const Home = () => {
         </Helmet>
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
+            className="h_bg-image  order-lg-2 h-100 margin-top-10 "
             style={{ backgroundImage: `url(${theme === 'dark' ? introdata.dark_url : introdata.light_url})` }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
